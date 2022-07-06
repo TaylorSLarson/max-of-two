05e7c497a3f87c531d32eed97b05f38ca47cdc5d
Taylor Larson <wilson.taylor635@gmail.com>
2022-07-05 20:31:17 -0600

    second commit

---
+++ maxOfTwo.js
@@ -0,0 +1,17 @@
+let x = 2
+let y = 2
+
+// Write your code below this line.
+
+if(x > y){
+    console.log('x')
+}else{
+    console.log(y)
+}
+
+
+if(x > y){
+    console.log(x)
+} else if(y > x){
+    console.log(y)
+}else{console.log ('the values are the same.')}
